3f9812c3e5730ab72a837a9f4d97796da5c41509
Ben Zhang <ben.zhang@salesforce.com>
2025-10-03 17:14:45 -0700

    keep a copy of sfdx standard project eslint config

---
+++ sfdxProject.eslint.config.js
@@ -0,0 +1,64 @@
+const { defineConfig } = require('eslint/config');
+const eslintJs = require('@eslint/js');
+const jestPlugin = require('eslint-plugin-jest');
+const auraConfig = require('@salesforce/eslint-plugin-aura');
+const lwcConfig = require('@salesforce/eslint-config-lwc/recommended');
+const globals = require('globals');
+
+module.exports = defineConfig([
+    // Aura configuration
+    {
+        files: ['**/aura/**/*.js'],
+        extends: [
+            ...auraConfig.configs.recommended,
+            ...auraConfig.configs.locker
+        ]
+    },
+
+    // LWC configuration
+    {
+        files: ['**/lwc/**/*.js'],
+
+         plugins: { 
+            "@salesforce/lwc-mobile": lwcMobilePlugin, 
+            "@salesforce/lwc-graph-analyzer": lwcGraphAnalyzerPlugin 
+        },
+        extends: [
+            lwcMobilePlugin.recommendedConfigs, 
+            lwcGraphAnalyzerPlugin.configs.recommended,
+            lwcConfig
+        ]
+    },
+
+    // LWC configuration with override for LWC test files
+    {
+        files: ['**/lwc/**/*.test.js'],
+        extends: [lwcConfig],
+        rules: {
+            '@lwc/lwc/no-unexpected-wire-adapter-usages': 'off'
+        },
+        languageOptions: {
+            globals: {
+                ...globals.node
+            }
+        }
+    },
+
+    // Jest mocks configuration
+    {
+        files: ['**/jest-mocks/**/*.js'],
+        languageOptions: {
+            sourceType: 'module',
+            ecmaVersion: 'latest',
+            globals: {
+                ...globals.node,
+                ...globals.es2021,
+                ...jestPlugin.environments.globals.globals
+            }
+        },
+        plugins: {
+            eslintJs
+        },
+        extends: ['eslintJs/recommended']
+    }
+]);
