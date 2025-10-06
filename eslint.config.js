fc91828628c1892a3ae9bf95830347d235b46a0b
Ben Zhang <ben.zhang@salesforce.com>
2025-10-06 11:04:48 -0700

    to match sfdx eslint config style

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,21 @@
-import eslintJs from '@eslint/js';
-import { defineConfig } from "eslint/config";
-import lwcMobilePlugin from "@salesforce/eslint-plugin-lwc-mobile";
-import lwcGraphAnalyzerPlugin from "@salesforce/eslint-plugin-lwc-graph-analyzer";
+const eslintJs = require("@eslint/js");
+const { defineConfig } = require("eslint/config");
+const lwcMobilePlugin = require("@salesforce/eslint-plugin-lwc-mobile");
+const lwcGraphAnalyzerPlugin = require("@salesforce/eslint-plugin-lwc-graph-analyzer");
 
-
-export default defineConfig([
-    eslintJs.configs.recommended,
-
-    // Salesforce LWC Mobile plugin configuration
-    { 
-        files: ["**/lwc/**/*.js"],
-        plugins: {
-            "eslintJs": eslintJs,
-            "@salesforce/lwc-mobile": lwcMobilePlugin, 
-            "@salesforce/lwc-graph-analyzer": lwcGraphAnalyzerPlugin 
-        },
-        extends: [
-            eslintJs.configs.recommended,
-            lwcMobilePlugin.recommendedConfigs, 
-            lwcGraphAnalyzerPlugin.configs.recommended
-        ],
-    }
+module.exports = defineConfig([
+  // Salesforce LWC Mobile plugin configuration
+  {
+    files: ["**/lwc/**/*.js"],
+    plugins: {
+      eslintJs: eslintJs,
+      "@salesforce/lwc-mobile": lwcMobilePlugin,
+      "@salesforce/lwc-graph-analyzer": lwcGraphAnalyzerPlugin
+    },
+    extends: [
+      eslintJs.configs.recommended,
+      lwcMobilePlugin.recommendedConfigs,
+      lwcGraphAnalyzerPlugin.configs.recommended
+    ]
+  }
 ]);
