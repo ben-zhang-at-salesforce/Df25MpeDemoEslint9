df7babfe3499fbaba3d6cd406beea720f59666e4
Ben Zhang <ben.zhang@salesforce.com>
2025-10-06 09:38:52 -0700

    more clean up

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,22 @@
-import js from '@eslint/js';
+import eslintJs from '@eslint/js';
 import { defineConfig } from "eslint/config";
 import lwcMobilePlugin from "@salesforce/eslint-plugin-lwc-mobile";
 import lwcGraphAnalyzerPlugin from "@salesforce/eslint-plugin-lwc-graph-analyzer";
 
 
 export default defineConfig([
-    js.configs.recommended,
+    eslintJs.configs.recommended,
 
     // Salesforce LWC Mobile plugin configuration
     { 
         files: ["**/lwc/**/*.js"],
-        plugins: { 
+        plugins: {
+            "eslintJs": eslintJs,
             "@salesforce/lwc-mobile": lwcMobilePlugin, 
             "@salesforce/lwc-graph-analyzer": lwcGraphAnalyzerPlugin 
         },
         extends: [
+            eslintJs.configs.recommended,
             lwcMobilePlugin.recommendedConfigs, 
             lwcGraphAnalyzerPlugin.configs.recommended
         ],
